ea5283a518d9c7afa995bff41285e8ca0199f141
Yotoko <vhgcuong95@gmail.com>
2024-03-20 17:31:56 +0700

    With redshift hooks

--- dags/dag_with_redshitf_hooks.py
+++ dags/dag_with_redshitf_hooks.py
@@ -1,7 +1,30 @@
 from datetime import datetime, timedelta
 from airflow.models import DAG
 
-from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator, RedshiftDataHook, RedshiftDataTrigger
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+
+create_table_sql = '''
+create table if not exists public.stock_quant_by_store ( id bigint not null,
+product_code varchar null,
+store_code varchar null,
+qty float8 null,
+qty_updated bool null default true,
+product_jan_1 varchar null,
+last_updated TIMESTAMP null,
+"level" int4 null,
+conversion_lvl_1_id int8 null,
+conversion_lvl_1_qty float8 null,
+to_lvl_1_qty float8 null,
+conversion_lvl_2_id int8 null,
+conversion_lvl_2_qty float8 null,
+to_lvl_2_qty float8 null,
+qty_pcs float8 null,
+qty_pcs_od float8 null,
+constraint stock_quant_by_store_product_code_store_code_key unique (product_code,
+store_code) );
+'''
 
 default_args = {
     'owner': 'cuongvh',
@@ -9,19 +32,68 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-with DAG(
-    dag_id=f"example_dag_redshift_v01",
+dag = DAG(
+    dag_id=f"run_query_in_redshift_v13",
     schedule="@daily",
     default_args=default_args,
-    start_date=datetime(2024, 3, 19),
+    start_date=datetime(2024, 3, 20, 0, 0, 0),
     max_active_runs=1,
-    catchup=False,
+    catchup=True,
     tags=["cuongvh", "airflow_2.6"]
-) as dag:
-    task1 = RedshiftDataTrigger(
-        aws_conn_id='aws_redshift'
+)
+
+def execute_redshift_query_and_display_result(**kwargs):
+    # Define your SQL query
+    sql_query = "SELECT * FROM public.stock_quant_by_store;"
+
+    # Create a PostgresHook to execute the query
+    redshift_hook = PostgresHook(postgres_conn_id='redshift_conn_id')  # Replace 'redshift_conn_id' with your connection ID
+    connection = redshift_hook.get_conn()
+    cursor = connection.cursor()
+
+    # Execute the query
+    cursor.execute(sql_query)
+
+    # Fetch and display the result
+    result = cursor.fetchall()
+    print("Query result:")
+    print(result)
+
+    # Close cursor and connection
+    cursor.close()
+    connection.close()
+
+def execute_insert_query():
+    sql_query = """
+        
+    """
+
+    redshift_hook = PostgresHook(postgres_conn_id='redshift_conn_id')  # Replace 'redshift_conn_id' with your connection ID
+    connection = redshift_hook.get_conn()
+    cursor = connection.cursor()
+
+    # Execute the query
+    cursor.execute(sql_query)
+
+    # Close cursor and connection
+    cursor.close()
+    connection.close()
+
+task1 = SQLExecuteQueryOperator(
+        task_id='create_table',
+        sql=create_table_sql,
+        dag=dag,
+        autocommit=True,
+        conn_id='aws_redshift_conn',
+        database='dev',
+        show_return_value_in_logs=True
     )
 
-    task2 = RedshiftDataHook(
+task2 = PythonOperator(
+    task_id='execute_redshift_query_and_display_result',
+    python_callable=execute_redshift_query_and_display_result,
+    provide_context=True,
+    dag=dag
+)
 
-    )
+task1 >> task2
